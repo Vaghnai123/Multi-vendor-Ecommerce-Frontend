ede8610534f4e2bb425f93c68b768d363f3e50e5
Vaghnai123 <vaghanijil8044@gmail.com>
2026-03-15 14:52:39 +0530

    updated something

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,30 @@
+// import axios from "axios";
+// const local = 'http://localhost:5000'
+
+// const api = axios.create({
+//     baseURL : `${local}/api`
+// })
+
+// export default api
+
 import axios from "axios";
-const local = 'http://localhost:5000'
+
+const local = 'http://localhost:5000';
+
+const production = 'https://multi-vendor-ecommerce-backend-vh2u.onrender.com'; 
+
+let api_url = '';
+let mode = 'pro'; 
+
+if (mode === 'pro') {
+    api_url = production;
+} else {
+    api_url = local;
+}
 
 const api = axios.create({
-    baseURL : `${local}/api`
-})
+    baseURL : `${api_url}/api`,
+    withCredentials: true 
+});
 
-export default api
+export default api;
